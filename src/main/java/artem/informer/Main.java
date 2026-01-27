3329716164a301393c09e72365f168c1030e9ce5
ArtemChurkin <a.churkin@informer.com>
2026-01-27 22:09:35 +0300

    Update Main.java

--- src/main/java/artem/informer/Main.java
+++ src/main/java/artem/informer/Main.java
@@ -5,9 +5,9 @@ package artem.informer;
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        // to see how IntelliJ IDEA suggests fixing it.  4543534534535
         System.out.printf("Hello and Goodbye!");
-        System.out.printf("Goodbye!");
+        System.out.printf("Goodbye!!!!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
